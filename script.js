f7754471011fb3c21be609bf715f9d9334a21dd8
Claw <bossclawest@outlook.com>
2025-04-10 11:36:49 -0400

    code: Add logic for Part 2 tasks 1-4, update progress.txt

--- script.js
+++ script.js
@@ -18,8 +18,25 @@ const student = {
 
 
   // P1.1(2) OUTPUT
-  console.log("Part 1 Output");
-  console.log(`Name ${student.name}`);
-  console.log(`Age  ${student.age}`);  
-  console.log(student.displayInfo());
+  // console.log("Part 1 Output");
+  // console.log(`Name ${student.name}`);
+  // console.log(`Age  ${student.age}`);  
+  // console.log(student.displayInfo());
+  
+
+  
+  // Part 2: Working with JSON
+  console.log("Part 2 Output");
+  const studentJSON = JSON.stringify(student);
+  console.log(`JSON String [${studentJSON}]`);
+  console.log();
+  
+  const newStudentObj = JSON.parse(studentJSON);
+  console.log(`Parsed Obj:`)
+  console.log(newStudentObj);
+  console.log();
+
+  let orig_matches_parsed = JSON.stringify(student) === JSON.stringify(newStudentObj);
+  console.log(`Orig matches the stringified and re-parsed? ${(orig_matches_parsed ? "Y" : "N")}`);
+  
   
